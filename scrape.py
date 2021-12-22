508438e4594230c845d314eb82016b10d423681a
nader <nado0or97@hotmail.com>
2021-12-22 23:47:45 +0100

    update link

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-page = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmaxtv-iptv-2020-2021/"
+page = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmaxtv-iptv-2020-2021-2/"
 
 def scrape_images():
     result = requests.get(page)
