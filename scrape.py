22f3f252f40b90c7b49384dd4a4a069256534f55
nader-alhalabi <nado0or97@hotmail.com>
2022-12-08 20:19:45 +0100

    change urls and images locations

--- scrape.py
+++ scrape.py
@@ -3,8 +3,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-page1 = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmaxtv-iptv-2020-2021/"
-page2 = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmaxtv-iptv-2020-2021-2/"
+page1 = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmax-2023-2022/"
+page2 = "https://www.vviruslove.com/2-%d9%83%d9%88%d8%af-%d8%aa%d9%81%d8%b9%d9%8a%d9%84-code-airmax-2023-2022-2/"
 
 # check which link has the code image
 def check_link():
@@ -24,8 +24,8 @@ def scrape_images():
     airmax = soup.find_all('img', {'src':re.compile(r'[0-9].jpg')})
     pro = soup.find_all('img', {'src':re.compile(r'[A-Z0-9].jpg')})
     #return airmax[0]["src"], pro[0]["src"]
-    final_code = re.findall(r'\b\d+\b', airmax[0]["src"])[2]
-    final_code2 = re.findall(r'[0-9A-Z]{6,6}', pro[0]["src"])[0]
+    final_code = re.findall(r'\b\d+\b', airmax[1]["src"])[2]
+    final_code2 = re.findall(r'[0-9A-Z]{6,6}', pro[2]["src"])[0]
     return final_code, final_code2
 
 
